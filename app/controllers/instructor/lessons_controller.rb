1f42d8fe7014be4b7d5bc14c545eac9e46369587
vgeroim <vgeroim.qa@gmail.com>
2019-08-17 21:48:14 +0000

    add permition on view and create for Section, Lesson only for Course owner

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,11 +1,19 @@
 class Instructor::LessonsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
     @section = Section.find(params[:section_id])
+      if @section.course.user != current_user
+        return render plain: 'Unauthorized', status: :unathorized
+      end
     @lesson = Lesson.new
   end
 
   def create
     @section = Section.find(params[:section_id])
+    if @section.course.user != current_user
+      return render plain: 'Unauthorized', status: :unathorized
+    end  
     @lesson = @section.lessons.create(lesson_params)
     redirect_to instructor_course_path(@section.course)
   end
--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,11 +1,19 @@
 class Instructor::SectionsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
     @course = Course.find(params[:course_id])
+    if @course.user != current_user
+      return render plain: 'Unauthorized', status: :unathorized
+    end 
     @section = Section.new
   end
 
   def create
     @course= Course.find(params[:course_id])
+    if @course.user != current_user
+      return render plain: 'Unauthorized', status: :unathorized
+    end
     @section = @course.sections.create(section_params)
     redirect_to instructor_course_path(@course)
   end
