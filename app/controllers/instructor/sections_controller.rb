edd05fc255d852d92297aa7db1db8d818fb8e5ce
vgeroim <vgeroim.qa@gmail.com>
2019-08-18 13:30:43 +0000

    require permission before action for Course and Section controller

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,24 +1,28 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_authorized_for_current_course
 
   def new
-    @course = Course.find(params[:course_id])
-    if @course.user != current_user
-      return render plain: 'Unauthorized', status: :unathorized
-    end 
     @section = Section.new
   end
 
   def create
-    @course= Course.find(params[:course_id])
-    if @course.user != current_user
-      return render plain: 'Unauthorized', status: :unathorized
-    end
-    @section = @course.sections.create(section_params)
-    redirect_to instructor_course_path(@course)
+    @section = current_course.sections.create(section_params)
+    redirect_to instructor_course_path(current_course)
   end
 
   private
+
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      return render plain: 'Unauthorized', status: :unathorized
+    end 
+  end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:course_id])
+  end
 
   def section_params
     params.require(:section).permit(:title)
