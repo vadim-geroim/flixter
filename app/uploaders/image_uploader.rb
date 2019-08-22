80623f771afc5e8b87ab4da904b4a60770cdc6c1
vgeroim <vgeroim.qa@gmail.com>
2019-08-22 00:41:35 +0000

    add MiniMagick gem to resize an image on a page

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   #storage :file
@@ -12,6 +12,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  process resize_to_fill: [800, 350]
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
