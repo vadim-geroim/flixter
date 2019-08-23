91c04e0d03e291266a7ad5bce919e59322d75560
Vadim <44911028+webqatutor@users.noreply.github.com>
2019-08-22 22:01:33 -0400

    add Image and Video uploading features (#3)
    * create show course details page
    
    * create Section model, controller and form
    
    * create Lesson model, controller and view to save lessons
    
    * add permition on view and create for Section, Lesson only for Course owner
    
    * refactor authorization before action for Lessons controller
    
    * require permission before action for Course and Section controller
    
    * create Lessons controller and Lesson details page
    
    * add CarrierWave to application and imageUploader to Course model
    
    * add Figaro gem and store images to S3 bucket
    
    * add MiniMagick gem to resize an image on a page
    
    * migrate from fog to carrierwave-aws
    
    * delete temp files from public/uploads and change aws_cl to be private
    
    * add VideoUploader to Lesson model
    
    * add VideoJS widget to Lesson show page

--- Gemfile
+++ Gemfile
@@ -25,6 +25,11 @@ gem 'jbuilder', '~> 2.5'
 gem 'jquery-rails'
 gem 'simple_form'
 gem 'devise'
+gem 'figaro'
+gem 'fog-aws'
+gem 'mini_magick'
+gem 'carrierwave', '~> 1.3.1'
+gem 'carrierwave-aws', '~> 1.0.2'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_17_181346) do
+ActiveRecord::Schema.define(version: 2019_08_23_005045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_08_17_181346) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
@@ -31,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_08_17_181346) do
     t.integer "section_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "video"
     t.index ["section_id"], name: "index_lessons_on_section_id"
   end
 
