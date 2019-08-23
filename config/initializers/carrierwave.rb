fcc18ca01016cec3729090c4adba453423bce35d
vgeroim <vgeroim.qa@gmail.com>
2019-08-23 00:30:36 +0000

    delete temp files from public/uploads and change aws_cl to be private

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@
 CarrierWave.configure do |config|
   config.storage    = :aws
   config.aws_bucket = ENV["AWS_BUCKET"]
-  config.aws_acl    = "public-read"
+  config.aws_acl    = "private"
 
   config.aws_credentials = {
       access_key_id:     ENV["AWS_ACCESS_KEY"],
