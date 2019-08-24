3d59a56748bc368ed4320124f6baed37e4861de2
vgeroim <vgeroim.qa@gmail.com>
2019-08-24 15:09:14 +0000

    add payment method with Stripe service on Course Page

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ApplicationRecord
+  mount_uploader :image, ImageUploader
+
   belongs_to :user
   has_many :sections
   has_many :enrollments
@@ -6,5 +8,13 @@ class Course < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
-  mount_uploader :image, ImageUploader
+  
+
+  def free?
+    cost.zero?
+  end
+
+  def premium?
+    ! free?
+  end
 end
