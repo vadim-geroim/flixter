6c4db63c3a1536125808dcceb67545e64d8070a4
vgeroim <vgeroim.qa@gmail.com>
2019-08-24 02:30:07 +0000

    add permission to view lesson only for enrolled couser

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson
+
   def show
   end
 
@@ -8,4 +11,12 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  def require_authorized_for_current_lesson
+    @course ||= current_lesson.section.course
+    unless current_user.enrolled_in?(@course)
+      redirect_to course_path(@course), alert: 'You are not enrolled in'
+    end
+  end
+
 end
