131c90284fd6a06d376a74f9e0110e0df300b40c
vgeroim <vgeroim.qa@gmail.com>
2019-08-31 05:30:29 +0000

    add row_order column to Sections and implement reoder sections on Course Page

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,6 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course
+  before_action :require_authorized_for_current_course, only: [:new, :create]
 
   def new
     @section = Section.new
@@ -9,6 +9,11 @@ class Instructor::SectionsController < ApplicationController
   def create
     @section = current_course.sections.create(section_params)
     redirect_to instructor_course_path(current_course)
+  end
+
+  def update
+    current_section.update_attributes(section_params)
+    render plain: 'updated!'
   end
 
   private
@@ -21,10 +26,18 @@ class Instructor::SectionsController < ApplicationController
 
   helper_method :current_course
   def current_course
-    @current_course ||= Course.find(params[:course_id])
+    if params[:course_id]
+      @current_course ||= Course.find(params[:course_id])
+    else
+      current_section.course
+    end
+  end
+
+  def current_section
+    @current_section ||= Section.find(params[:id])
   end
 
   def section_params
-    params.require(:section).permit(:title)
+    params.require(:section).permit(:title, :row_order_position)
   end
 end
