1e46c957e0005a441b81f43e701316bfad7b950f
vgeroim <vgeroim.qa@gmail.com>
2019-09-07 20:44:30 +0000

    add privacy page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
   resource :dashboard, only: [:show]
+  get 'privacy', to: 'static_pages#privacy'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: [:create]
   end
